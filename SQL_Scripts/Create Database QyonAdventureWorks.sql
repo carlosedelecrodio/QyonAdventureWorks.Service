699b7e9ba1879dd0d674890b39130dfc2f4a5c57
Carlos Eduardo Levi Delecrodio <125407505+carlosedelecrodio@users.noreply.github.com>
2023-02-16 00:48:10 -0300

    Script criação do banco

---
+++ SQL_Scripts/Create Database QyonAdventureWorks.sql
@@ -0,0 +1,12 @@
+
+<!-- Criação do banco de dados -->
+
+CREATE DATABASE "QyonAdventureWorks"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
